26ca59c1d1aa4bcd1008efb8851de60cabd6c04e
Lukas <76838159+wolflu05@users.noreply.github.com>
2025-06-27 13:55:12 +0200

    fix issue with plugin interface and ci + bump version (#5)
    * fix issue with plugin interface and bump version
    
    * fix ci

--- inventree_template_editor/InvenTreeTemplateEditorPlugin.py
+++ inventree_template_editor/InvenTreeTemplateEditorPlugin.py
@@ -1,11 +1,9 @@
-from django.conf import settings
-from django.utils.translation import gettext_lazy as _
+import os
+
 from rest_framework.request import Request
 
 from plugin import InvenTreePlugin
 from plugin.mixins import UserInterfaceMixin
-from plugin.base.ui.mixins import FeatureType, UIFeature
-from plugin.templatetags.plugin_extras import plugin_static
 
 from .version import TEMPLATE_EDITOR_PLUGIN_VERSION
 
@@ -16,26 +14,24 @@ class InvenTreeTemplateEditorPlugin(InvenTreePlugin, UserInterfaceMixin):
     VERSION = TEMPLATE_EDITOR_PLUGIN_VERSION
 
     # UI Plugins were added with inventree 0.17.0
-    MIN_VERSION = "0.17.0"
+    # Interface was updated in 0.18.0
+    MIN_VERSION = "0.18.0"
 
     TITLE = "InvenTree Template Editor Plugin"
     SLUG = "inventree-template-editor-plugin"
     NAME = "InvenTreeTemplateEditorPlugin"
 
-    def get_ui_features(self, feature_type: FeatureType, context: dict, request: Request) -> list[UIFeature]:
-        if feature_type != "template_editor" or context.get("template_type", None) != "labeltemplate":
+    def get_ui_template_editors(self, request: Request, context, **kwargs):
+        if context.get('template_type') != 'labeltemplate':
             return []
-
-        is_dev = settings.CUSTOMIZE.get("inventree_template_editor_plugin_dev", False)
 
         return [
             {
-                "feature_type": "template_editor",
-                "options": {
-                    "key": "label-designer",
-                    "title": _("Label Designer"),
-                    "icon": "build"
-                },
-                "source": plugin_static({}, "labelEditor.dev.js" if is_dev else "dist/labelEditor.js", plugin="inventree-template-editor-plugin"),
+                "key": "label-designer",
+                "title": "Label Designer",
+                "icon": "ti:tools:outline",
+                "source": self.plugin_static_file(
+                    "labelEditor.dev.js" if os.environ.get('INVENTREE_TEMPLATE_EDITOR_DEV', False) else "dist/labelEditor.js"
+                ),
             }
         ]
